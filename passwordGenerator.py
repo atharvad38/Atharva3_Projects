9b34cf2aac9b54c5f5d533d17c31b47ce0a34209
Atharva Deshmukh <149713134+atharvad38@users.noreply.github.com>
2024-01-17 00:04:46 +0530

    Add files via upload

---
+++ passwordGenerator.py
@@ -0,0 +1,42 @@
+import random
+
+random_letters1 = ''.join(random.choices('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',k=5))
+
+
+def easy_Password(n):
+    random_letters1 = ''.join(random.choices('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',k=n))
+    print(random_letters1)
+
+
+def medium_password(n):
+    random_letters2 = ''.join(random.choices('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789',k=n))
+    print(random_letters2)
+
+def hard_password(n):
+     random_letters3 = ''.join(random.choices('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789-+*/!@#$%&',k=n))
+     print(random_letters3)
+
+
+choice = 0
+while choice!=4:
+    print("\nWelcome to Password generator")
+    print("1.Easy level password")
+    print("2.Medium level password")
+    print("3.Difficult level password")
+    print("4.EXIT")
+    choice = int(input("Choose any one option:"))
+    match choice:
+        case 1:
+            n = int(input("Enter the length of password:"))
+            easy_Password(n)
+        case 2:
+            m = int(input("Enter the length of password:"))
+            medium_password(m)
+        case 3:
+            l = int(input("Enter the length of password:"))
+            hard_password(l)
+        case 4:
+            print("EXITING.....")
+        case _:
+            print("Invalid choice")
+
